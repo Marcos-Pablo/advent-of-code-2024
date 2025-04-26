1acc3adc10a4ec4684bf40ca453b62eb9d0c1460
Marcos Pablo <marquin.pablo@gmail.com>
2025-04-26 17:55:02 -0300

    Adding day 3 solution

---
+++ day3/main.py
@@ -0,0 +1,38 @@
+import re
+from pathlib import Path
+
+def extract_valid_instructions():
+    matches = []
+    script_dir = Path(__file__).parent
+    input_path = script_dir / "input.txt"
+    with open(input_path.resolve(), "r") as file:
+        for line in file:
+            matches += re.findall("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)", line)
+    return matches
+
+def process_instructions(instructions):
+    res = 0
+    res_with_tokens = 0
+    is_enabled = True
+    for instruction in instructions:
+        if instruction == "do()":
+            is_enabled = True
+        elif instruction == "don't()":
+            is_enabled = False
+        else:
+            num1, num2 = re.findall("\\d{1,3}", instruction)
+            product = int(num1) * int(num2)
+            res += product
+            if is_enabled:
+                res_with_tokens += product
+
+    return res, res_with_tokens
+
+def main():
+    instructions = extract_valid_instructions()
+    res, res_with_tokens = process_instructions(instructions)
+
+    print(f"result without considering tokens -> {res}")
+    print(f"result considering tokens -> {res_with_tokens}")
+
+main()
