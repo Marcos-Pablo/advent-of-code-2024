234cc2bff2cb57e21553b2565f7f8c88804d6517
Marcos Pablo <marquin.pablo@gmail.com>
2025-04-28 20:14:41 -0300

    Adding benchmark for the day3 solution

--- day3/main.py
+++ day3/main.py
@@ -1,5 +1,7 @@
 import re
 from pathlib import Path
+import time
+import tracemalloc
 
 def extract_valid_instructions():
     matches = []
@@ -29,10 +31,17 @@ def process_instructions(instructions):
     return res, res_with_tokens
 
 def main():
+    start = time.perf_counter()
+    tracemalloc.start()
     instructions = extract_valid_instructions()
     res, res_with_tokens = process_instructions(instructions)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    end = time.perf_counter()
 
     print(f"result without considering tokens -> {res}")
     print(f"result considering tokens -> {res_with_tokens}")
+    print(f"Elapsed time: {end - start: .6f} second(s)")
+    print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
 
 main()
