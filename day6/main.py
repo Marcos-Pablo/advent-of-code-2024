590585ca60d6740d2fbeff72f539549136fb84fd
Marcos Pablo <marquin.pablo@gmail.com>
2025-04-28 22:45:24 -0300

    Adding day 6 part 1 solution

---
+++ day6/main.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import time
+import tracemalloc
+
+def extract_matrix_and_starting_point():
+    script_dir = Path(__file__).parent
+    input_path = script_dir / "input.txt"
+    matrix = []
+    starting_point = None
+    with open(input_path.resolve(), "r") as file:
+        for line in file:
+            matrix.append([])
+            for i in range(len(line) - 1):
+                c = line[i]
+                if c == "^":
+                    starting_point = (len(matrix) - 1, i)
+                    matrix[-1].append(".")
+                else:
+                    matrix[-1].append(c)
+    return matrix, starting_point
+
+def get_new_position(i, j, direction):
+    if direction == 0:
+        return i - 1, j
+    elif direction == 1:
+        return i, j + 1
+    elif direction == 2:
+        return i + 1, j
+    else:
+        return i, j - 1
+
+def trace_patrol_path(matrix, starting_point):
+    n, m = len(matrix), len(matrix[0])
+    i, j = starting_point
+    visited = set()
+    direction = 0
+    while True:
+        visited.add((i, j))
+        new_row, new_col = get_new_position(i, j, direction)
+        if new_row < 0 or new_row >= n or new_col < 0 or new_col >= m:
+            break
+
+        if matrix[new_row][new_col] == ".":
+            i, j = new_row, new_col
+        else:
+            direction = (direction + 1) % 4
+    return len(visited)
+
+def main():
+    start = time.perf_counter()
+    tracemalloc.start()
+
+    matrix, starting_point = extract_matrix_and_starting_point()
+    positions_visited = trace_patrol_path(matrix, starting_point)
+
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    end = time.perf_counter()
+
+    print(f"Part 1 response: {positions_visited}")
+    print(f"Elapsed time: {end - start: .6f} second(s)")
+    print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
+
+main()
