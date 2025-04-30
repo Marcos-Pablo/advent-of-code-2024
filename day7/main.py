c86490ea2d702d96d4314176806ac49d4a0e9d74
Marcos Pablo <marquin.pablo@gmail.com>
2025-04-29 22:28:39 -0300

    Adding day 7 part 2 solution

--- day7/main.py
+++ day7/main.py
@@ -17,7 +17,7 @@ def extract_equations():
             equations[test_value].append(values)
     return equations
 
-def is_equation_valid(test_value, values):
+def solve_part_one(test_value, values):
     def backtracking(val, i):
         if i == len(values):
             return val == test_value
@@ -28,27 +28,42 @@ def is_equation_valid(test_value, values):
         )
     return backtracking(values[0], 1)
 
+def solve_part_two(test_value, values):
+    def backtracking(val, i):
+        if i == len(values):
+            return val == test_value
+        
+        return (
+            backtracking(val * values[i], i + 1) or 
+            backtracking(val + values[i], i + 1) or
+            backtracking(int(str(val) + str(values[i])), i + 1)
+        )
+    return backtracking(values[0], 1)
+
 def validate_equations(equations):
-    res = 0
+    res1 = 0
+    res2 = 0
     for test_value, grouped_values in equations.items():
         for values in grouped_values:
-            if is_equation_valid(test_value, values):
-                res += test_value
-    return res
+            if solve_part_one(test_value, values):
+                res1 += test_value
+            if solve_part_two(test_value, values):
+                res2 += test_value
+    return res1, res2
 
 def main():
     start = time.perf_counter()
     tracemalloc.start()
     
     equations = extract_equations()
-    res = validate_equations(equations)
+    res1, res2 = validate_equations(equations)
 
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
     end = time.perf_counter()
 
-    print(f"Part 1 response: {res}")
-    print(f"Part 2 response: ")
+    print(f"Part 1 response: {res1}")
+    print(f"Part 2 response: {res2}")
     print(f"Elapsed time: {end - start: .6f} second(s)")
     print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
 
