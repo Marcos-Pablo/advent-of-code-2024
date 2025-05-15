1c65287d00bd716233ff869e1e39485da7147a33
Marcos Pablo <marquin.pablo@gmail.com>
2025-05-14 21:32:29 -0300

    Fixing day 16 part 1 solution

--- day16/main.py
+++ day16/main.py
@@ -3,11 +3,17 @@ import time
 import tracemalloc
 import heapq
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 def extract_maze():
     script_dir = Path(__file__).parent
     input_path = script_dir / "input.txt"
     with open(input_path.resolve(), "r") as file:
         maze = [[line[i] for i in range(len(line) - 1)] for line in file]
+    print(maze)
     return maze
 
 def print_maze(maze):
@@ -24,20 +30,17 @@ def get_reindeer_pos(maze):
 def get_neighbours(maze, i, j, direction):
     n, m = len(maze), len(maze[0])
     neighbours = []
-    cost_up = float("inf")
-    cost_right = float("inf")
-    cost_down = float("inf")
-    cost_left = float("inf")
+    cost_up = cost_down = cost_right = cost_left = float("inf")
 
-    if direction == "^":
+    if direction == UP:
         cost_up = 1
         cost_left = 1001
         cost_right = 1001
-    elif direction == ">":
+    elif direction == RIGHT:
         cost_right = 1
         cost_up = 1001
         cost_down = 1001
-    elif direction == "v":
+    elif direction == DOWN:
         cost_down = 1
         cost_right = 1001
         cost_left = 1001
@@ -47,36 +50,38 @@ def get_neighbours(maze, i, j, direction):
         cost_up = 1001
 
     if i - 1 >= 0 and maze[i - 1][j] != "#":
-        neighbours.append((i - 1, j, cost_up, "^"))
+        neighbours.append((i - 1, j, cost_up, UP))
 
     if j - 1 >= 0 and maze[i][j - 1] != "#":
-        neighbours.append((i, j - 1, cost_left, "<"))
+        neighbours.append((i, j - 1, cost_left, LEFT))
 
     if j + 1 < m and maze[i][j + 1] != "#":
-        neighbours.append((i, j + 1, cost_right, ">"))
+        neighbours.append((i, j + 1, cost_right, RIGHT))
 
     if i + 1 < n and maze[i + 1][j] != "#":
-        neighbours.append((i + 1, j, cost_down, "v"))
+        neighbours.append((i + 1, j, cost_down, DOWN))
 
     return neighbours
 
 def calc_min_score_to_get_out_of_the_maze(maze):
+    m = len(maze[0])
     reindeer_row, reindeer_col = get_reindeer_pos(maze)
     visited = set()
-    heap = [(0, ">", reindeer_row, reindeer_col)]
-    count = 0
+    heap = [(0, RIGHT, reindeer_row, reindeer_col)]
     while heap:
         total_cost, direction, i, j = heapq.heappop(heap)
-        if (i, j) in visited:
+        key = get_hash_key(i, j, direction, m)
+        if key in visited:
             continue
-        count += 1
         if maze[i][j] == "E":
-            print(count)
             return total_cost
-        visited.add((i, j))
+        visited.add(key)
         for row, col, cost, direction in get_neighbours(maze, i, j, direction):
             heapq.heappush(heap, (total_cost + cost, direction, row, col))
     return float("inf")
+
+def get_hash_key(row, col, direction, m):
+    return (row * m + col) * 4 + direction
 
 def main():
     print("Processing input...")
