f34f4741a240e7fcec73cda197c1c4d68ebda21d
Marcos Pablo <marquin.pablo@gmail.com>
2025-07-27 21:36:09 -0300

    adding day 18 part 1 solution

---
+++ day18/main.py
@@ -0,0 +1,96 @@
+from pathlib import Path
+from collections import deque
+import time
+import tracemalloc
+
+NUM_ROWS = 7
+NUM_COLS = 7
+NUM_BYTES = 12
+
+def extract_matrix():
+    script_dir = Path(__file__).parent
+    input_path = script_dir / "input.txt"
+    matrix = [["." for _ in range(NUM_COLS)] for _ in range(NUM_ROWS)]
+    with open(input_path.resolve(), "r") as file:
+        for _ in range(NUM_BYTES):
+            coords = file.readline().split(",")
+            j, i = int(coords[0]), int(coords[1])
+            matrix[i][j] = "#"
+    return matrix
+
+def print_matrix(matrix):
+    for line in matrix:
+        print(" ".join(line))
+
+def get_neighbours(i, j, matrix):
+    moves = [
+        (-1, 0),
+        (0, 1),
+        (1, 0),
+        (0, -1)
+    ]
+
+    neighbours = []
+
+    for m1, m2 in moves:
+        new_row, new_col = i + m1, j + m2
+        if (
+            new_row < 0 or new_row >= NUM_ROWS or
+            new_col < 0 or new_col >= NUM_COLS or
+            matrix[new_row][new_col] == "#"
+        ):
+            continue
+        neighbours.append((new_row, new_col))
+
+    return neighbours
+
+def find_shortest_path(matrix):
+    q = deque([(0, 0)])
+    visited = set([(0, 0)])
+    dist = 0
+    while q:
+        for _ in range(len(q)):
+            i, j = q.popleft()
+
+            if i == NUM_ROWS - 1 and j == NUM_COLS - 1:
+                return dist
+
+            for new_row, new_col in get_neighbours(i, j, matrix):
+                if (new_row, new_col) in visited:
+                    continue
+                q.append((new_row, new_col))
+                visited.add((new_row, new_col))
+
+        dist += 1
+
+def main():
+    print("Processing input...")
+    tracemalloc.start()
+    start = time.perf_counter()
+    matrix = extract_matrix()
+    end = time.perf_counter()
+    current, peak = tracemalloc.get_traced_memory()
+    print_matrix(matrix)
+
+    print(f"Elapsed time: {end - start: .6f} second(s)")
+    print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
+    print("==================================")
+
+    start = time.perf_counter()
+    shortest_path = find_shortest_path(matrix)
+    end = time.perf_counter()
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Response part 1: {shortest_path}")
+    print(f"Elapsed time: {end - start: .6f} second(s)")
+    print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
+    print("==================================")
+
+    start = time.perf_counter()
+
+    end = time.perf_counter()
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Response part 2: ")
+    print(f"Elapsed time: {end - start: .6f} second(s)")
+    print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
+
+main()
