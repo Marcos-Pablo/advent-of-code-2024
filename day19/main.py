7ca6f0d05bf6d7eeb073f0095e24026594e6306a
Marcos Pablo <marquin.pablo@gmail.com>
2025-07-29 12:39:11 -0300

    adding day 19 part 2 solution

--- day19/main.py
+++ day19/main.py
@@ -11,21 +11,34 @@ def process_input():
         designs = blocks[1].strip().split("\n")
         return available_patterns, designs
 
-def can_form_pattern(rem, available_patterns):
-    if not rem:
-        return True
-    for i in range(len(rem)):
-        pattern = rem[:i + 1]
-        if pattern in available_patterns and can_form_pattern(rem[i + 1:], available_patterns):
-            return True
-    return False
+def find_combinations(target_design, available_patterns):
+    cache = {}
+    def find_combinations_r(target_design):
+        if target_design in cache:
+            return cache[target_design]
 
-def find_answer_part1(designs, available_patterns):
+        if not target_design:
+            return 1
+
+        combinations = 0
+        for i in range(len(target_design)):
+            pattern = target_design[:i + 1]
+            if pattern in available_patterns:
+                new_target_design = target_design[i + 1:]
+                combinations += find_combinations_r(new_target_design)
+        cache[target_design] = combinations
+        return combinations
+    return find_combinations_r(target_design)
+
+def find_answers(designs, available_patterns):
     number_of_possible_patterns = 0
+    number_of_possible_combinations = 0
     for design in designs:
-        if can_form_pattern(design, available_patterns):
+        combinations = find_combinations(design, available_patterns)
+        if combinations:
             number_of_possible_patterns += 1
-    return number_of_possible_patterns
+            number_of_possible_combinations += combinations
+    return number_of_possible_patterns, number_of_possible_combinations
 
 def main():
     print("Processing input...")
@@ -40,13 +53,13 @@ def main():
 
     tracemalloc.start()
     start = time.perf_counter()
-    number_of_possible_patterns = find_answer_part1(designs, available_patterns)
+    number_of_possible_patterns, number_of_possible_combinations = find_answers(designs, available_patterns)
     end = time.perf_counter()
     current, peak = tracemalloc.get_traced_memory()
     print(f"Response part 1: {number_of_possible_patterns}")
+    print(f"Response part 2: {number_of_possible_combinations}")
     print(f"Elapsed time: {end - start: .6f} second(s)")
     print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
     print("==================================")
 
-    print(f"Response part 2: ")
 main()
