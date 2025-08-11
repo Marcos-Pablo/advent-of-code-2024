f9f32d3cb6cb4a7008e5448a398d5158d110b765
Marcos Pablo <marquin.pablo@gmail.com>
2025-08-11 12:25:45 -0300

    adding day 20 part 1 solution

--- day20/main.py
+++ day20/main.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import defaultdict, deque
 from pathlib import Path
 import time
 import tracemalloc
@@ -13,7 +13,8 @@ def extract_map():
         for line in file:
             j = 0
             new_line = []
-            for ch in line:
+            for k in range(len(line) - 1):
+                ch = line[k]
                 new_line.append(ch)
                 if ch == "S":
                     start_pos = (i, j)
@@ -24,15 +25,13 @@ def extract_map():
             map.append(new_line)
     return map, start_pos, end_pos
 
-def print_map(map, start_pos, end_pos):
+def print_map(map):
+    # Calculate max width for each column
+    col_widths = [max(len(str(row[i])) for row in map) for i in range(len(map[0]))]
     for line in map:
-        print(" ".join(line))
+        print(" ".join(str(cell).ljust(col_widths[i]) for i, cell in enumerate(line)))
 
-    print(f"start_pos -> {start_pos}")
-    print(f"end_pos -> {end_pos}")
-
-def get_neighbours(map, i, j):
-    neighbours = []
+def get_next_pos(map, i, j):
     moves = [
         (-1, 0),
         (1, 0),
@@ -43,38 +42,76 @@ def get_neighbours(map, i, j):
     for m1, m2 in moves:
         new_row, new_col = i + m1, j + m2
         if (
-            new_row < 0 or new_row >= len(map) or
-            new_col < 0 or new_col >= len(map[0]) or
-            map[new_row][new_col] == "#"
+            0 <= new_row < len(map) and
+            0 <= new_col < len(map[0]) and
+            (
+                map[new_row][new_col] == "." or 
+                map[new_row][new_col] == "E"
+            )
+        ):
+            return (new_row, new_col)
+
+    return None
+
+def get_shortcuts(map, i, j):
+    shortcuts = []
+    moves = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1)
+    ]
+
+    for m1, m2 in moves:
+        cheat_start_row, cheat_start_col = i + m1, j + m2
+        cheat_end_row, cheat_end_col = cheat_start_row + m1, cheat_start_col + m2
+        if (
+            cheat_end_row < 0 or cheat_end_row >= len(map) or
+            cheat_end_col < 0 or cheat_end_col >= len(map[0]) or
+            map[cheat_start_row][cheat_start_col] != "#" or
+            map[cheat_end_row][cheat_end_col] == "#"
         ):
             continue
-        neighbours.append((new_row, new_col))
-    return neighbours
 
-def fill_map_with_original_time(map, start_pos):
-    visited = set([start_pos])
-    q = deque([start_pos])
-    
+        shortcuts.append((cheat_end_row, cheat_end_col))
+    return shortcuts
+
+def calc_original_path(map, start_pos):
+    original_path = []
     picoseconds = 0
-    while q:
-        i, j = q.popleft()
+    next_pos = start_pos
+    while next_pos:
+        i, j = next_pos
+        original_path.append((i, j))
         map[i][j] = str(picoseconds)
-
-        for new_row, new_col in get_neighbours(map, i, j):
-            if (new_row, new_col) in visited:
-                continue
-            q.append((new_row, new_col))
-            visited.add((new_row, new_col))
         picoseconds += 1
+        next_pos = get_next_pos(map, i, j)
+    return original_path
+
+def calc_cheats(map, original_path):
+    cheats = defaultdict(lambda:0)
+    for i, j in original_path:
+        shortcuts = get_shortcuts(map, i, j)
+        for cheat_end_row, cheat_end_col in shortcuts:
+            diff_in_picoseconds = int(map[cheat_end_row][cheat_end_col]) - int(map[i][j]) - 2
+            if diff_in_picoseconds > 0:
+                cheats[diff_in_picoseconds] += 1
+    return cheats
+
+def calc_num_of_cheats_that_saves_at_least_100_picoseconds(cheats):
+    num_of_cheats_that_saves_at_least_100_picoseconds = 0
+    for picoseconds_saved, count in cheats.items():
+        if picoseconds_saved >= 100:
+            num_of_cheats_that_saves_at_least_100_picoseconds += count
+    return num_of_cheats_that_saves_at_least_100_picoseconds
 
 def main():
     print("Processing input...")
     tracemalloc.start()
     start = time.perf_counter()
+
     map, start_pos, end_pos = extract_map()
-    print_map(map, start_pos, end_pos)
-    fill_map_with_original_time(map, start_pos)
-    print_map(map, start_pos, end_pos)
+
     end = time.perf_counter()
     current, peak = tracemalloc.get_traced_memory()
     print(f"Elapsed time: {end - start: .6f} second(s)")
@@ -84,9 +121,12 @@ def main():
     tracemalloc.start()
     start = time.perf_counter()
 
+    original_path = calc_original_path(map, start_pos)
+    cheats = calc_cheats(map, original_path)
+    num_of_cheats_that_saves_at_least_100_picoseconds = calc_num_of_cheats_that_saves_at_least_100_picoseconds(cheats)
     end = time.perf_counter()
     current, peak = tracemalloc.get_traced_memory()
-    print(f"Response part 1: ")
+    print(f"Response part 1: {num_of_cheats_that_saves_at_least_100_picoseconds}")
     print(f"Response part 2: ")
     print(f"Elapsed time: {end - start: .6f} second(s)")
     print(f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB")
