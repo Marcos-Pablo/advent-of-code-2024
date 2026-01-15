899b8c9a44fb4127cafa896f6fca11d28221f29a
Marcos Pablo <marquin.pablo@gmail.com>
2026-01-15 00:02:24 -0300

    add day 22 part 2 solution

--- day22/main.py
+++ day22/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import time
 import tracemalloc
+from collections import defaultdict, deque
 
 
 def extract_secrets():
@@ -8,7 +9,7 @@ def extract_secrets():
     input_path = script_dir / "input.txt"
     with open(input_path.resolve(), "r") as file:
         codes = map(int, file.read().splitlines())
-        return codes
+        return list(codes)
 
 
 def mix(secret, val):
@@ -44,6 +45,38 @@ def get_next_secret(secret):
     return secret
 
 
+def calc_total_secrets_sum(secrets):
+    secrets_sum = 0
+    for secret in secrets:
+        next_secret = secret
+        for _ in range(2000):
+            next_secret = get_next_secret(next_secret)
+        secrets_sum += next_secret
+    return secrets_sum
+
+
+def calc_total_bananas(secrets):
+    totals = defaultdict(int)
+    for start in secrets:
+        window = deque(maxlen=4)
+        seen = set()
+        s = start
+        prev_price = s % 10
+        for _ in range(2000):
+            s = get_next_secret(s)
+            price = s % 10
+            diff = price - prev_price
+            prev_price = price
+            window.append(diff)
+            if len(window) == 4:
+                sequence = tuple(window)
+                if sequence not in seen:
+                    totals[sequence] += price
+                    seen.add(sequence)
+                window.popleft()
+    return max(totals.values())
+
+
 def main():
     print("Processing input...")
     tracemalloc.start()
@@ -62,15 +95,20 @@ def main():
     tracemalloc.start()
     start = time.perf_counter()
 
-    secrets_sum = 0
-    for secret in secrets:
-        next_secret = secret
-        for _ in range(2000):
-            next_secret = get_next_secret(next_secret)
-        secrets_sum += next_secret
+    print(f"Response part 1: {calc_total_secrets_sum(secrets)}")
 
-    print(f"Response part 1: {secrets_sum}")
-    print(f"Response part 2: ")
+    end = time.perf_counter()
+    current, peak = tracemalloc.get_traced_memory()
+    print(f"Elapsed time: {end - start: .6f} second(s)")
+    print(
+        f"Current memory usage: {current / 10**6:.6f} MB; Peak was {peak / 10**6:.6f} MB"
+    )
+    print("==================================")
+
+    tracemalloc.start()
+    start = time.perf_counter()
+
+    print(f"Response part 2: {calc_total_bananas(secrets)}")
 
     end = time.perf_counter()
     current, peak = tracemalloc.get_traced_memory()
